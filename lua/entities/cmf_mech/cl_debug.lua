2dc7e9fc5d53303ef4fd81b3634c6a1a70334570
TankNut <m.vlasblom@live.nl>
2025-08-10 01:44:20 +0200

    Formatting

--- lua/entities/cmf_mech/cl_debug.lua
+++ lua/entities/cmf_mech/cl_debug.lua
@@ -4,19 +4,19 @@ local drawHitboxes = CreateClientConVar("cmf_debug_hitboxes", 0)
 local drawLegs = CreateClientConVar("cmf_debug_legs", 0)
 
 local physicsColor = Color(255, 191, 0)
-local hitboxColor = Color(0, 161, 255)
+local hitboxColor =  Color(0, 161, 255)
 
 local forward = Color(255, 0, 0)
-local right = Color(0, 255, 0)
-local up = Color(0, 0, 255)
+local right =   Color(0, 255, 0)
+local up =      Color(0, 0, 255)
 
 local length = 10
 
 function ENT:DrawDebug()
-	if drawPhysics:GetBool() then self:DrawPhysics() end
-	if drawBones:GetBool() then self:DrawBones() end
+	if drawPhysics:GetBool()  then self:DrawPhysics() end
+	if drawBones:GetBool()    then self:DrawBones() end
 	if drawHitboxes:GetBool() then self:DrawHitboxes() end
-	if drawLegs:GetBool() then self:DrawLegs() end
+	if drawLegs:GetBool()     then self:DrawLegs() end
 end
 
 function ENT:DrawPhysics()
@@ -38,15 +38,15 @@ end
 function ENT:DrawLegs()
 	for k, leg in pairs(self.Legs) do
 		render.DrawLine(leg.Hip.Pos, leg.Hip.Pos + leg.Hip.Ang:Forward() * self.UpperLength, forward)
-		render.DrawLine(leg.Hip.Pos, leg.Hip.Pos + leg.Hip.Ang:Right() * length, right)
-		render.DrawLine(leg.Hip.Pos, leg.Hip.Pos + leg.Hip.Ang:Up() * length, up)
+		render.DrawLine(leg.Hip.Pos, leg.Hip.Pos + leg.Hip.Ang:Right()   * length,           right)
+		render.DrawLine(leg.Hip.Pos, leg.Hip.Pos + leg.Hip.Ang:Up()      * length,           up)
 
 		render.DrawLine(leg.Knee.Pos, leg.Knee.Pos + leg.Knee.Ang:Forward() * self.LowerLength, forward)
-		render.DrawLine(leg.Knee.Pos, leg.Knee.Pos + leg.Knee.Ang:Right() * length, right)
-		render.DrawLine(leg.Knee.Pos, leg.Knee.Pos + leg.Knee.Ang:Up() * length, up)
+		render.DrawLine(leg.Knee.Pos, leg.Knee.Pos + leg.Knee.Ang:Right()   * length,           right)
+		render.DrawLine(leg.Knee.Pos, leg.Knee.Pos + leg.Knee.Ang:Up()      * length,           up)
 
 		render.DrawLine(leg.Foot.Pos, leg.Foot.Pos + leg.Foot.Ang:Forward() * length, forward)
-		render.DrawLine(leg.Foot.Pos, leg.Foot.Pos + leg.Foot.Ang:Right() * length, right)
-		render.DrawLine(leg.Foot.Pos, leg.Foot.Pos + leg.Foot.Ang:Up() * length, up)
+		render.DrawLine(leg.Foot.Pos, leg.Foot.Pos + leg.Foot.Ang:Right()   * length, right)
+		render.DrawLine(leg.Foot.Pos, leg.Foot.Pos + leg.Foot.Ang:Up()      * length, up)
 	end
 end
