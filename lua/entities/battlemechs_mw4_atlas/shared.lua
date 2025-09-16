2806960370e59e92d1fdf4f08007754b1bb20acc
TankNut <m.vlasblom@live.nl>
2025-09-16 04:55:11 +0200

    Atlas tweaking

--- lua/entities/battlemechs_mw4_atlas/shared.lua
+++ lua/entities/battlemechs_mw4_atlas/shared.lua
@@ -11,24 +11,24 @@ ENT.Spawnable = true
 
 -- Movement related fields
 ENT.Hull = {-- The size of the mech's collision hull, this is only used for movement. Keep it off the ground so it doesn't catch on any bumps
-	Mins = Vector(-64, -64, -5),
-	Maxs = Vector(64, 64, 105)
+	Mins = Vector(-52, -52, -35),
+	Maxs = Vector(52, 52, 145)
 }
 
 ENT.GroundOffset = 160 -- How far above the ground the mech sits
 
-ENT.MoveAcceleration = 200 -- Hammer units/s of acceleration when moving or slowing down
+ENT.MoveAcceleration = 100 -- Hammer units/s of acceleration when moving or slowing down
 ENT.MaxSlope = 40 -- The steepest slope a mech can walk on, any steeper and it'll start sliding down
 
-ENT.WalkSpeed = 200 -- The target speed when walking normally
-ENT.RunSpeed = 600 -- The target speed when +speed is held down
+ENT.WalkSpeed = 160 -- The target speed when walking normally
+ENT.RunSpeed = 300 -- The target speed when +speed is held down
 
 ENT.TurnRate = 70 -- [MoveStat] Degrees/s the mech can turn
 
 -- Gait/leg related fields
-ENT.StepSize = 360 -- [MoveStat] The forward length of each step the mech takes
-ENT.Stance = {0.45, 0.65} -- [MoveStat] Fraction of time each leg spends in the air
-ENT.ForwardLean = {1.1, 1.3} -- [MoveStat] How far off-center the mech's legs are when moving
+ENT.StepSize = 320 -- [MoveStat] The forward length of each step the mech takes
+ENT.Stance = {0.45, 0.55} -- [MoveStat] Fraction of time each leg spends in the air
+ENT.ForwardLean = 1.1 -- [MoveStat] How far off-center the mech's legs are when moving
 
 ENT.SideStep = 10 -- [MoveStat] The amount of side offset that's applied to the gait offset value
 ENT.UpStep = {1.5, 1} -- [MoveStat] The amount of upwards offset that's applied to the gait offset value
