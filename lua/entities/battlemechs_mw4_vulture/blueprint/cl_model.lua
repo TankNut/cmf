0e2500fa6b1caad1a52b7bb2de338e8f29e8b253
TankNut <m.vlasblom@live.nl>
2025-09-16 02:27:20 +0200

    Update vulture model files

--- lua/entities/battlemechs_mw4_vulture/blueprint/cl_model.lua
+++ lua/entities/battlemechs_mw4_vulture/blueprint/cl_model.lua
@@ -1,35 +1,35 @@
 ENT.ModelData = {
 	-- Hip
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_hip.mdl", "Root"),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_hip.mdl", "Root"),
 
 	-- Torso
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_torso.mdl", "Torso", {Pos = Vector(-10, 0, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_specia.mdl", "Chin", {Pos = Vector(0, 0, 13.5)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_torso.mdl", "Torso", {Pos = Vector(-10, 0, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_chin.mdl", "Chin", {Pos = Vector(0, 0, 13.5)}),
 
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_specia_left.mdl", "ChinGun", {Pos = Vector(0, 14, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_specia_right.mdl", "ChinGun", {Pos = Vector(0, -14, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_chin_left.mdl", "ChinGun", {Pos = Vector(0, 14, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_chin_right.mdl", "ChinGun", {Pos = Vector(0, -14, 0)}),
 
 	-- Weapons
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_lgun.mdl", "LeftWeapon", {Pos = Vector(0, -10, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_rgun.mdl", "RightWeapon", {Pos = Vector(0, 10, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_gun_left.mdl", "LeftWeapon", {Pos = Vector(0, -10, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_gun_right.mdl", "RightWeapon", {Pos = Vector(0, 10, 0)}),
 
 	-- Left leg
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_luleg.mdl", "LeftHip",  {Pos = Vector(0, -11, 0), Ang = Angle(-124, 0, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_ldleg.mdl", "LeftKnee", {Pos = Vector(0, 1, 0), Ang = Angle(-62, 0, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_lfoot.mdl", "LeftFoot", {Pos = Vector(0, 0, 33)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_upperleg_left.mdl", "LeftHip",  {Pos = Vector(0, -11, 0), Ang = Angle(-124, 0, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_lowerleg_left.mdl", "LeftKnee", {Pos = Vector(0, 1, 0), Ang = Angle(-62, 0, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_foot_left.mdl", "LeftFoot", {Pos = Vector(0, 0, 33)}),
 
 	-- Left foot
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_litoe.mdl", "LeftFoot", {Pos = Vector(12.5, -11.25, 10.6), Ang = Angle(0, -45)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_lotoe.mdl", "LeftFoot", {Pos = Vector(12.5,  11.25, 10.6), Ang = Angle(0,  45)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_lbtoe.mdl", "LeftFoot", {Pos = Vector(-20,  0, 10.6), Ang = Angle(0,  0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_toe_left_inner.mdl", "LeftFoot", {Pos = Vector(12.5, -11.25, 10.6)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_toe_left_outer.mdl", "LeftFoot", {Pos = Vector(12.5,  11.25, 10.6)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_toe_left_back.mdl", "LeftFoot", {Pos = Vector(-20,  0, 10.6)}),
 
 	-- Right leg
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_ruleg.mdl", "RightHip",  {Pos = Vector(0, 11, 0), Ang = Angle(-124, 0, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_rdleg.mdl", "RightKnee", {Pos = Vector(0, -1, 0), Ang = Angle(-60, 0, 0)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_rfoot.mdl", "RightFoot", {Pos = Vector(0, 0, 33)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_upperleg_right.mdl", "RightHip",  {Pos = Vector(0, 11, 0), Ang = Angle(-124, 0, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_lowerleg_right.mdl", "RightKnee", {Pos = Vector(0, -1, 0), Ang = Angle(-60, 0, 0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_foot_right.mdl", "RightFoot", {Pos = Vector(0, 0, 33)}),
 
 	-- Right foot
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_ritoe.mdl", "RightFoot", {Pos = Vector(12.5,  11.25, 10.6), Ang = Angle(0,  45)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_rotoe.mdl", "RightFoot", {Pos = Vector(12.5, -11.25, 10.6), Ang = Angle(0, -45)}),
-	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vtr_rbtoe.mdl", "RightFoot", {Pos = Vector(-20,  0, 10.6), Ang = Angle(0,  0)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_toe_right_inner.mdl", "RightFoot", {Pos = Vector(12.5,  11.25, 10.6)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_toe_right_outer.mdl", "RightFoot", {Pos = Vector(12.5, -11.25, 10.6)}),
+	battlemechs:ModelPart("models/battlemechs/mw4/vulture/vulture_toe_right_back.mdl", "RightFoot", {Pos = Vector(-20,  0, 10.6)}),
 }
