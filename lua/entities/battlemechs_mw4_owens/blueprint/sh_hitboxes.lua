8ccf69951285b42e5bd35f0d383110032dcc8733
TankNut <m.vlasblom@live.nl>
2025-09-24 02:00:55 +0200

    Fix owens and vulture weapon hitboxes

--- lua/entities/battlemechs_mw4_owens/blueprint/sh_hitboxes.lua
+++ lua/entities/battlemechs_mw4_owens/blueprint/sh_hitboxes.lua
@@ -6,8 +6,8 @@ if SERVER then
 	self:AddHitbox("Torso", Vector(2.5, 0, -5), Angle(-90, 0, 0), Vector(55, 40, 40))
 	self:AddHitbox("Torso", Vector(-20, 0, 13), Angle(0, 0, 0),   Vector(40, 100, 15))
 
-	self:AddHitbox("LeftWeapon", Vector(-28, 65, 1),  Angle(0, 0, -90), Vector(60, 47, 30))
-	self:AddHitbox("RightWeapon", Vector(-28, -65, 1), Angle(0, 0, 90),  Vector(60, 47, 30))
+	self:AddHitbox("LeftWeapon", Vector(-28, 11, 5),  Angle(0, 0, -90), Vector(60, 47, 30))
+	self:AddHitbox("RightWeapon", Vector(-28, -11, 5), Angle(0, 0, 90),  Vector(60, 47, 30))
 
 	self:AddHitbox("LHip",  Vector(-13, -12, 5), Angle(0, 0, 0),   Vector(self.UpperLength + 25, 13, 30))
 	self:AddHitbox("LKnee", Vector(-10, 0, -5),  Angle(-10, 0, 0), Vector(self.LowerLength + 10, 20, 30))
--- lua/entities/battlemechs_mw4_vulture/blueprint/sh_hitboxes.lua
+++ lua/entities/battlemechs_mw4_vulture/blueprint/sh_hitboxes.lua
@@ -5,8 +5,8 @@ if SERVER then
 		self:AddHitbox("Torso", Vector(-75, 0, 35), Angle(0, 0, 0), Vector(160, 80, 110))
 		self:AddHitbox("Torso", Vector(-30, 0, 23), Angle(0, 0, 0), Vector(15, 120, 15))
 
-		self:AddHitbox("LeftWeapon", Vector(-52, 12.5, -5), Angle(0, 0, 0), Vector(110, 25, 40))
-		self:AddHitbox("RightWeapon", Vector(-52, -12.5, -5), Angle(0, 0, 0), Vector(110, -25, 40))
+		self:AddHitbox("LeftWeapon", Vector(-52, 2.5, -5), Angle(0, 0, 0), Vector(110, 25, 40))
+		self:AddHitbox("RightWeapon", Vector(-52, -2.5, -5), Angle(0, 0, 0), Vector(110, -25, 40))
 
 		self:AddHitbox("LeftHip", Vector(-13, 0, 2), Angle(0, 0, 0), Vector(90, 13, 35))
 		self:AddHitbox("LeftKnee", Vector(-35, -2, -28),  Angle(-17, 0, 0), Vector(80, 20, 15))
